a8a7db6599ec57c86c165201949f46dbfbedd936
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:58:31 -0700

    Create MatchingQuestion Stub File

---
+++ MatchingQuestion.java
@@ -0,0 +1,25 @@
+/**
+ *
+ *
+ */
+
+import java.util.ArrayList;
+
+class MatchingQuestion extends 
+
+Question
+{
+    private ArrayList<Option> mOptions;
+    
+    public 
+
+ArrayList<Option> getOptions()
+    {
+    }
+    
+    public boolean isRight
+
+(string pAnswer)
+    {
+    }
+}
