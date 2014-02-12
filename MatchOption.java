80bd3583e18efac7dc77e1e3018d10815e395666
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:56:57 -0700

    Create MatchOption Stub File

---
+++ MatchOption.java
@@ -0,0 +1,22 @@
+/**
+ *
+ *
+ */
+
+class MatchOption implements Option
+{
+    private String mTerm;
+    
+
+private String mDef;
+    
+    public ChoiceOption(String pTerm, String pDef)
+ 
+
+   {
+    }
+
+    public String[] getDisplay()
+    {
+    }
+}
