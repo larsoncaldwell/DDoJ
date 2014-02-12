381e05ebbc2adbb2833a83bf5e56083a8b99bee5
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:52:32 -0700

    Create Quiz Stub File

---
+++ Quiz.java
@@ -0,0 +1,28 @@
+/**
+ *
+ *
+ *
+ */
+
+import java.util.ArrayList;
+
+class Quiz
+{
+    private ArrayList<Question> mQuestions;
+
+    public Quiz()
+    {
+    }
+
+    public ArrayList<Question> getQuestions()
+    {
+    }
+
+    public void setQuestions(ArrayList<Question> pQuestions)
+    {
+    }
+
+    public void addQuestion(Question pQuestion)
+    {
+    }
+}
