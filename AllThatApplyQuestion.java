c954965c694054267afb80fd1d4f247be4e22450
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:57:48 -0700

    Create AllThatApplyQuestion Stub File

---
+++ AllThatApplyQuestion.java
@@ -0,0 +1,26 @@
+/**
+ *
+ *
+ */
+
+import java.util.ArrayList;
+
+class AllThatApplyQuestion extends 
+
+Question
+{
+    private ArrayList<Option> mOptions;
+    private 
+
+ArrayList<String> mAnswers;
+    
+    public ArrayList<Option> getOptions()
+   
+
+ {
+    }
+    
+    public boolean isRight(string pAnswer)
+    {
+    }
+}
