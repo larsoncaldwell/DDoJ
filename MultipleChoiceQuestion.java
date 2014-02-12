9675d07f70dac280c31100cf5bdf245de1907585
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:55:25 -0700

    Create MultipleChoiceQuestion Stub File

---
+++ MultipleChoiceQuestion.java
@@ -0,0 +1,20 @@
+/**
+ *
+ *
+ */
+
+import java.util.ArrayList;
+
+class MultipleChoiceQuestion extends Question
+{
+    private ArrayList<Option> mOptions;
+    private String mAnswer;
+    
+    public ArrayList<Option> getOptions()
+    {
+    }
+    
+    public boolean isRight(string pAnswer)
+    {
+    }
+}
