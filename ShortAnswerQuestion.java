8547cc4231d8afe614a4b566449020b2d91e3c59
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:56:15 -0700

    Create ShortAnswerQuestion Stub File

---
+++ ShortAnswerQuestion.java
@@ -0,0 +1,19 @@
+/**
+ *
+ *
+ */
+
+import java.util.ArrayList;
+
+class ShortAnswerQuestion extends Question
+{
+    private ArrayList<String> mAnswers;
+    
+    public ArrayList<Option> getOptions()
+    {
+    }
+    
+    public boolean isRight(string pAnswer)
+    {
+    }
+}
