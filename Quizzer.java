740b90a0ffab0afe7fa01c999805db6426e1795d
Haru McClellan <haruphoenix@gmail.com>
2014-02-11 20:51:54 -0700

    Create Quizzer Stub File

---
+++ Quizzer.java
@@ -0,0 +1,27 @@
+/**
+ * This is the stub of what will be a simple quizzing application.
+ * It will read a file and support the types of questions specified
+ * in QuizFormat.txt i.e. True/False, Multiple Choice, Check all
+ * that apply, Matching, and Short Answer. 
+ */
+
+class Quizzer
+{
+    private Quiz currentQuiz;
+
+    public static void main(String[] args)
+    {
+    }
+
+    public void startQuiz()
+    {
+    }
+
+    public void loadQuiz()
+    {
+    }
+
+    public void scoreQuiz()
+    {
+    }
+}
