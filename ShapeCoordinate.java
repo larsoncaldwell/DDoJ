fd3fc3bf9cd556f0a6781171a6cf69042d57f07c
Haru McClellan <haruphoenix@gmail.com>
2014-02-17 16:32:48 -0700

    Create ShapeCoordinate.java

---
+++ ShapeCoordinate.java
@@ -0,0 +1,32 @@
+
+class ShapeCoordinate
+{
+    private int mX;
+    private int mY;
+
+    public ShapeCoordinate(int pX, int pY)
+    {
+	mX = pX;
+	mY = pY;
+    }
+
+    public void setX(int pX)
+    {
+	mX = pX;
+    }
+    
+    public void setY(int pY)
+    {
+	mY = pY;
+    }
+
+    public int getX()
+    {
+	return mX;
+    }
+
+    public int getY()
+    {
+	return mY;
+    }
+}
